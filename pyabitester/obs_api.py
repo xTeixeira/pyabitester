7674afa800e8e44115c03241c1694171b01eabb9
Antonio Teixeira <antonio.teixeira@suse.com>
2024-11-22 15:29:08 -0300

    obs_api: implement get_binary, binary_list and repo_list

--- pyabitester/obs_api.py
+++ pyabitester/obs_api.py
@@ -8,6 +8,48 @@ class ObsApi():
         self.auth = auth
         self.session = requests.Session()
 
+    def get_binary(self, project_name, repository_name, architecture_name, package_name, file_name):
+        get_binary_url = f"/build/{project_name}/{repository_name}/{architecture_name}/{package_name}/{file_name}"
+        response = self.session.get(self.url + get_binary_url, verify=False, auth=self.auth)
+        if response.headers['content-type'] == "application/x-rpm":
+            return response.content
+        return None
+
+    def binary_list(self, project_name, repository_name, architecture_name, package_name):
+        '''
+        <binarylist>
+            <binary filename="hello-2.10-3.1.x86_64.rpm" size="58352" mtime="1617270174">
+            </binary>
+        </binarylist>
+        '''
+        binary_list_url = f"/build/{project_name}/{repository_name}/{architecture_name}/{package_name}"
+        response = self.session.get(self.url + binary_list_url, verify=False, auth=self.auth)
+        print(binary_list_url)
+        print (response.text)
+        binary_filename_list = []
+        binarylist = ET.fromstring(response.text)
+        for binary in binarylist:
+            print(binary.attrib)
+            binary_filename_list.append(binary.attrib["filename"])
+        return binary_filename_list
+
+    def repo_list(self, project_name):
+        '''
+        <directory>
+            <entry name="openSUSE_Tumbleweed">
+            </entry>
+            <entry name="openSUSE_Leap_15.3">
+            </entry>
+        </directory>
+        '''
+        repo_list_url = f"/build/{project_name}"
+        response = self.session.get(self.url + repo_list_url, verify=False, auth=self.auth)
+        repo_list = []
+        directory = ET.fromstring(response.text)
+        for entry in directory:
+            repo_list.append(entry.attrib["name"])
+        return repo_list
+
     def meta_pkg(self, project_name, package_name):
         '''
         <package name="package_1">
