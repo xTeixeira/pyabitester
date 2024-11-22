2327bb17215eec9d3b3466233c42d94f2300d3c0
Antonio Teixeira <antonio.teixeira@suse.com>
2024-11-22 15:28:02 -0300

    obs_api: Add api response examples, change variable naming

--- pyabitester/obs_api.py
+++ pyabitester/obs_api.py
@@ -8,19 +8,35 @@ class ObsApi():
         self.auth = auth
         self.session = requests.Session()
 
-    def meta_pkg(self, project, package_name):
-        # Actually only returns "name" and "project" fields
-        meta_pkg_url = "/source/{}/{}/_meta".format(project, package_name)
+    def meta_pkg(self, project_name, package_name):
+        '''
+        <package name="package_1">
+            <title></title>
+            <description>Test package</description>
+            <devel project="home:Admin" package="package_2">
+            </devel>
+        </package>
+        '''
+        # Actually only returns "name" and "project" (if existent) fields
+        meta_pkg_url = f"/source/{project_name}/{package_name}/_meta"
         response = self.session.get(self.url + meta_pkg_url, verify=False, auth=self.auth)
         meta_xml = ET.fromstring(response.text)
         return meta_xml.attrib
 
-    def showlinked(self, project, package_name):
-        showlinked_url = "/source/{}/{}?cmd=showlinked".format(project, package_name)
+    def showlinked(self, project_name, package_name):
+        '''
+        <collection>
+            <package name="hello_world" project="home:Admin:branches:home:Admin">
+            </package>
+            <package name="hello_world2" project="home:Admin:branches:home:Admin">
+            </package>
+        </collection>
+        '''
+        showlinked_url = f"/source/{project_name}/{package_name}?cmd=showlinked"
         response = self.session.post(self.url + showlinked_url, verify=False, auth=self.auth)
         linked_package_list = []
-        packages_xml = ET.fromstring(response.text)
-        for package in packages_xml:
+        collection = ET.fromstring(response.text)
+        for package in collection:
             linked_package_list.append(package.attrib)
 
         return linked_package_list
